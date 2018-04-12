38e4d5ef0451fb5af3b4226c14ad2829fbeb154a
Nate Imig <nimig@autonomoustuff.com>
2018-04-12 16:02:49 -0500

    Corrected formatting & removed deadzone parameter in launch file

--- src/startup_checks.cpp
+++ src/startup_checks.cpp
@@ -136,8 +136,12 @@ bool AS::Joystick::check_controller_type(ros::NodeHandle * nodeH)
     {
       PublishControl::controller = LOGITECH_G29;
 			
-			PublishControl::max_rot_rad = 7.85;
-			
+      // Set to match the G29 controller's max center-to-lock steering angle (radians).
+      PublishControl::max_rot_rad = 7.85;
+      
+
+
+
       // steering wheel, not right stick
       PublishControl::axes[RIGHT_STICK_LR] = 0;
       // throttle pedal, not right trigger
