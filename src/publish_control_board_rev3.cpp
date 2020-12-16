1d167283ee23789e2bb3d8d57ae467450a84f9e1
Amruta Mhaske <amhaske@autonomoustuff.com>
2020-12-16 13:23:40 -0500

    fixed accel cmd for ftt

--- src/publish_control_board_rev3.cpp
+++ src/publish_control_board_rev3.cpp
@@ -256,7 +256,8 @@ void PublishControlBoardRev3::publish_accelerator_message(const sensor_msgs::Joy
       if (vehicle_type == LEXUS_RX_450H ||
           vehicle_type == VEHICLE_4 ||
           vehicle_type == VEHICLE_5 ||
-          vehicle_type == VEHICLE_6)
+          vehicle_type == VEHICLE_6 ||
+          vehicle_type == VEHICLE_FTT)
       {
         accelerator_cmd_pub_msg.command = accel_scale_val * (0.5 * (msg->axes[axes[RIGHT_TRIGGER_AXIS]] + 1.0));
       }
@@ -278,7 +279,8 @@ void PublishControlBoardRev3::publish_accelerator_message(const sensor_msgs::Joy
       if (vehicle_type == LEXUS_RX_450H ||
           vehicle_type == VEHICLE_4 ||
           vehicle_type == VEHICLE_5 ||
-          vehicle_type == VEHICLE_6)
+          vehicle_type == VEHICLE_6 ||
+          vehicle_type == VEHICLE_FTT)
       {
         accelerator_cmd_pub_msg.command = accel_scale_val * (-0.5 * (msg->axes[axes[RIGHT_TRIGGER_AXIS]] - 1.0));
       }
