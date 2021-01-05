a59037ffcf9cde4dceb84286d6701274abef63c2
Amruta Mhaske <amhaske@autonomoustuff.com>
2021-01-05 15:54:27 -0500

    added hcv

--- src/publish_control_board_rev3.cpp
+++ src/publish_control_board_rev3.cpp
@@ -257,7 +257,8 @@ void PublishControlBoardRev3::publish_accelerator_message(const sensor_msgs::Joy
           vehicle_type == VEHICLE_4 ||
           vehicle_type == VEHICLE_5 ||
           vehicle_type == VEHICLE_6 ||
-          vehicle_type == VEHICLE_FTT)
+          vehicle_type == VEHICLE_FTT ||
+          vehicle_type == VEHICLE_HCV)
       {
         accelerator_cmd_pub_msg.command = accel_scale_val * (0.5 * (msg->axes[axes[RIGHT_TRIGGER_AXIS]] + 1.0));
       }
@@ -280,7 +281,8 @@ void PublishControlBoardRev3::publish_accelerator_message(const sensor_msgs::Joy
           vehicle_type == VEHICLE_4 ||
           vehicle_type == VEHICLE_5 ||
           vehicle_type == VEHICLE_6 ||
-          vehicle_type == VEHICLE_FTT)
+          vehicle_type == VEHICLE_FTT ||
+          vehicle_type == VEHICLE_HCV)
       {
         accelerator_cmd_pub_msg.command = accel_scale_val * (-0.5 * (msg->axes[axes[RIGHT_TRIGGER_AXIS]] - 1.0));
       }
@@ -358,7 +360,8 @@ void PublishControlBoardRev3::publish_lights_horn_wipers_message(const sensor_ms
 {
   if ((vehicle_type == LEXUS_RX_450H ||
        vehicle_type == VEHICLE_5 ||
-       vehicle_type == VEHICLE_FTT) &&
+       vehicle_type == VEHICLE_FTT ||
+      vehicle_type == VEHICLE_HCV) &&
       controller != HRI_SAFE_REMOTE)
   {
     pacmod_msgs::SystemCmdInt headlight_cmd_pub_msg;
