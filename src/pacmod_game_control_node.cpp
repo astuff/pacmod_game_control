a6a4124cef66e988ad1fcd2e83712a6ef2aca14d
Ian Colwell <ian.colwell@hexagon.com>
2021-09-28 13:55:15 -0400

    Update copyright year

--- src/pacmod_game_control_node.cpp
+++ src/pacmod_game_control_node.cpp
@@ -1,5 +1,5 @@
 /*
- * Unpublished Copyright (c) 2009-2018 AutonomouStuff, LLC, All Rights Reserved.
+ * Unpublished Copyright (c) 2009-2021 AutonomouStuff, LLC, All Rights Reserved.
  *
  * This file is part of the PACMod ROS 1.0 driver which is released under the MIT license.
  * See file LICENSE included with this software or go to https://opensource.org/licenses/MIT for full license details.
