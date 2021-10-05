489c9e42042f13afebbc2036a0f1253704694bf6
Ian Colwell <ian.colwell@hexagon.com>
2021-10-05 11:47:14 -0400

    Switch to single-threaded, testing requried

--- src/pacmod_game_control_node.cpp
+++ src/pacmod_game_control_node.cpp
@@ -12,16 +12,17 @@
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "pacmod_game_control");
-  ros::AsyncSpinner spinner(2);
-  ros::NodeHandle priv("~");
+  // ros::AsyncSpinner spinner(2);
 
-  ros::Time::waitForValid();
+  // ros::Time::waitForValid();
 
   GameControl pub_control;
   pub_control.init();
 
-  spinner.start();
-  ros::waitForShutdown();
+  ros::spin();
+
+  // spinner.start();
+  // ros::waitForShutdown();
 
   return 0;
 }
